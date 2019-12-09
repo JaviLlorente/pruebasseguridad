68a4ccb6eba660df790497dad6e912a20244d6d0
JaviLlorente <58568865+JaviLlorente@users.noreply.github.com>
2019-12-09 14:04:54 +0100

    Change sheets URL and no use AwesomeMarkers
    - index.html (references to AwesomeMarkers have been commented).
    - leaflet-gsheets.js (basemap changed, some functions of the polygons have been commented and the points have been built without using AwesomeMarkers).
    Some png markers had been added from: https://github.com/pointhi/leaflet-color-markers

--- leaflet-gsheets.js
+++ leaflet-gsheets.js
@@ -11,8 +11,8 @@ function init() {
   // PASTE YOUR URLs HERE
   // these URLs come from Google Sheets 'shareable link' form
   // the first is the polygon layer and the second the points
-  var polyURL = 'https://docs.google.com/spreadsheets/d/1EUFSaqi30b6oefK0YWWNDDOzwmCTTXlXkFHAc2QrUxM/edit?usp=sharing';
-  var pointsURL = 'https://docs.google.com/spreadsheets/d/1kjJVPF0LyaiaDYF8z_x23UulGciGtBALQ1a1pK0coRM/edit?usp=sharing';
+  var polyURL = 'https://docs.google.com/spreadsheets/d/1IUvPxkzFq-150l_tNod22UjmDiD2qZqRFyV3DU3ObUs/edit?usp=sharing';
+  var pointsURL = 'https://docs.google.com/spreadsheets/d/1vAwVVkXdXVDvUKsMqRcwEI_rmr9t1q8Z-PLNiKncB1k/edit?usp=sharing';
 
   Tabletop.init( { key: polyURL,
     callback: addPolygons,
@@ -24,13 +24,12 @@ function init() {
 window.addEventListener('DOMContentLoaded', init);
 
 // Create a new Leaflet map centered on the continental US
-var map = L.map('map').setView([40, -100], 4);
+var map = L.map('map').setView([41.08, -3.97], 9.5);
 
 // This is the Carto Positron basemap
-var basemap = L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}{r}.png', {
-  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
-  subdomains: 'abcd',
-  maxZoom: 19
+var basemap = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
+	maxZoom: 18
 });
 basemap.addTo(map);
 
@@ -96,31 +95,31 @@ function addPolygons(data) {
   }
 
   // The polygons are styled slightly differently on mouse hovers
-  var polygonStyle = {'color': '#2ca25f', 'fillColor': '#99d8c9', 'weight': 1.5};
-  var polygonHoverStyle = {'color': 'green', 'fillColor': '#2ca25f', 'weight': 3};
+  var polygonStyle = {'color': '#2ca25f', 'fillColor': '#FFFFFF80', 'weight': 1.5};
+  //var polygonHoverStyle = {'color': 'green', 'fillColor': '#2ca25f', 'weight': 3};
 
   polygonLayer = L.geoJSON(geojsonStates, {
-    onEachFeature: function (feature, layer) {
-      layer.on({
-        mouseout: function(e) {
-          e.target.setStyle(polygonStyle);
-        },
-        mouseover: function(e) {
-          e.target.setStyle(polygonHoverStyle);
-        },
-        click: function(e) {
-          // This zooms the map to the clicked polygon
+    //onEachFeature: function (feature, layer) {
+      //layer.on({
+        //mouseout: function(e) {
+        //  e.target.setStyle(polygonStyle);
+        //},
+        //mouseover: function(e) {
+        //  e.target.setStyle(polygonHoverStyle);
+        //},
+        //click: function(e) {
+          // Esto acerca el mapa al polígono seleccionado
           // map.fitBounds(e.target.getBounds());
 
           // if this isn't added, then map.click is also fired!
-          L.DomEvent.stopPropagation(e);
+          //L.DomEvent.stopPropagation(e);
 
-          document.getElementById('sidebar-title').innerHTML = e.target.feature.properties.name;
-          document.getElementById('sidebar-content').innerHTML = e.target.feature.properties.summary;
-          sidebar.open(panelID);
-        }
-      });
-    },
+          //document.getElementById('sidebar-title').innerHTML = e.target.feature.properties.name;
+          //document.getElementById('sidebar-content').innerHTML = e.target.feature.properties.summary;
+          //sidebar.open(panelID);
+        //}
+      //});
+    //},
     style: polygonStyle
   }).addTo(map);
 }
@@ -134,34 +133,65 @@ function addPoints(data) {
   pointGroupLayer = L.layerGroup().addTo(map);
 
   for(var row = 0; row < data.length; row++) {
-    var marker = L.marker([data[row].lat, data[row].long]).addTo(pointGroupLayer);
+    var marker = L.marker([data[row].Latitud, data[row].Longitud], {
+		title: (data[row].Especie+ ' / ' + data[row].Fecha),
+		//draggable:false,
+		opacity: 1
+	}).addTo(pointGroupLayer);
 
     // UNCOMMENT THIS LINE TO USE POPUPS
-    //marker.bindPopup('<h2>' + data[row].location + '</h2>There's a ' + data[row].level + ' ' + data[row].category + ' here');
+    // marker.bindPopup(data[row].N + ' / ' + data[row].Especie + ' / ' + data[row].Fecha);
 
     // COMMENT THE NEXT 14 LINES TO DISABLE SIDEBAR FOR THE MARKERS
     marker.feature = {
       properties: {
-        location: data[row].location,
-        category: data[row].category
+		N: data[row].N, 
+		Usuario: data[row].Usuario,
+        Clase: data[row].Clase,
+        Especie: data[row].Especie,
+		Fecha: data[row].Fecha,
+		Seguridad_id: data[row].Seguridad_id,
+		Frecuencia_paso: data[row].Frecuencia_paso,
+		Carretera: data[row].Carretera,
+		Pk: data[row].Pk,
+		Foto: data[row].Foto,
+		Observaciones: data[row].Observaciones,
       }
     };
     marker.on({
       click: function(e) {
         L.DomEvent.stopPropagation(e);
-        document.getElementById('sidebar-title').innerHTML = e.target.feature.properties.location;
-        document.getElementById('sidebar-content').innerHTML = e.target.feature.properties.category;
+        document.getElementById('sidebar-title').innerHTML = e.target.feature.properties.Especie;
+        document.getElementById('sidebar-content').innerHTML = (
+			'N: ' + e.target.feature.properties.N + '<br/>' +
+			'Usuario: ' + e.target.feature.properties.Usuario + '<br/>' +
+			'Fecha: ' + e.target.feature.properties.Fecha + '<br/>' +
+			//'Clase: ' + e.target.feature.properties.Clase + '<br/>' +
+			//'Especie: ' + e.target.feature.properties.Especie + '<br/>' +
+			'Seguridad_id: ' + e.target.feature.properties.Seguridad_id + '<br/>' +
+			'Frecuencia_paso: ' + e.target.feature.properties.Frecuencia_paso + '<br/>' +
+			'Carretera: ' + e.target.feature.properties.Carretera + '<br/>' +	
+			'Pk: ' + e.target.feature.properties.Pk + '<br/>' +
+			//'Foto: ' + e.target.feature.properties.Foto + '<br/>' +
+			'Observaciones: ' + e.target.feature.properties.Observaciones + '<br/>' +
+			'<img src="' + e.target.feature.properties.Foto + '" width="300">' 
+			);		
+		
         sidebar.open(panelID);
       }
     });
 
-    // AwesomeMarkers is used to create fancier icons
-    var icon = L.AwesomeMarkers.icon({
-      icon: 'info-sign',
-      iconColor: 'white',
-      markerColor: getColor(data[row].category),
-      prefix: 'glyphicon',
-      extraClasses: 'fa-rotate-0'
+	//<i class='fas fa-map-marker-alt' style='font-size:24px;color:red'></i>
+	
+    // For Awesome icons use, see master branch
+    var icon = L.icon({
+    iconUrl: getIcon(data[row].Clase),
+    iconSize: [20, 30],
+    iconAnchor: [10, 30],
+    popupAnchor: [0, -30],
+    //shadowUrl: 'css/images/markers-shadow.png',
+    //shadowSize: [30, 10],
+    //shadowAnchor: [5, 5]
     });
     marker.setIcon(icon);
   }
@@ -169,13 +199,17 @@ function addPoints(data) {
 
 // Returns different colors depending on the string passed
 // Used for the points layer
-function getColor(type) {
+  function getIcon(type) {
   switch (type) {
-    case 'Coffee Shop':
-      return 'green';
-    case 'Restaurant':
-      return 'blue';
+    case 'MAMIFERO':
+      return 'css/images/marker-icon-red.png';
+    case 'AVE':
+      return 'css/images/marker-icon-yellow.png';
+	 case 'REPTIL':
+      return 'css/images/marker-icon-green.png';
+	 case 'ANFIBIO':
+     return 'css/images/marker-icon-blue.png';
     default:
-      return 'green';
+      return 'pink';
   }
 }
