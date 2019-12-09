f8a253c4da16f21a09fb4c8c365bb6c68e4e1612
JaviLlorente <58568865+JaviLlorente@users.noreply.github.com>
2019-12-09 17:49:07 +0100

    Update leaflet-gsheets.js

--- leaflet-gsheets.js
+++ leaflet-gsheets.js
@@ -24,7 +24,7 @@ function init() {
 window.addEventListener('DOMContentLoaded', init);
 
 // Create a new Leaflet map centered on the continental US
-var map = L.map('map').setView([41.16, -4.00], 9.4);
+var map = L.map('map').setView([41.14, -4.00], 9.4);
 
 // This is the Carto Positron basemap
 var basemap = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
